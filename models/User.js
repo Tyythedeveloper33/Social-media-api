3822d85ef3138e8bb2a83553867bf1bd0a7248dd
unknown <kingwebbe@gmail.com>
2023-09-22 20:37:43 -0400

    got objects to come back for friends , thoughts have to figure out how to make it so the id for the friends list &  id for the thoughts pop up in the objects

--- models/User.js
+++ models/User.js
@@ -1,16 +1,24 @@
 // Require schema and model from mongoose
 const mongoose = require('mongoose');
-
+const thoughtSchema = new mongoose.Schema({
+  title: { type: String, required: false },
+ 
+});
+const friendsSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+ 
+});
 // Construct a new instance of the schema class
 const UserSchema = new mongoose.Schema({
   // Configure individual properties using Schema Types
   username: { type: String, required: true },
-  thoughtText: { type: String, required: false },
+  thoughts: { type: String, required: false },
   // The type of data is set to 'String' and required is set to false, meaning it will accept null values
- reactions: { type: String, required: false },
-  
+  friends: [friendsSchema],
+  thoughts: [thoughtSchema],
   reactionCount: Number,
   // Use built in date method to get current date
+  email:{ type: String, required: true }, 
   createdat: { type: Date, default: Date.now },
 });
 
@@ -19,13 +27,13 @@ const Users = mongoose.model('MyUser', UserSchema);
 
 // Create new instances of the model, a document
 Users.create([
-  { username: 'tyythegoon',thoughtText: "im in my own world", },
-  { username: 'enphanamus', thoughtText: "im in my own world"},
-  { username: '2cups',thoughtText: "im in my own world"},
-  { username: 'diamond2flatt', thoughtText: "im in my own world" },
-  { username: 'gotmonion',thoughtText: "im in my own world" },
-  { username: 'petty',thoughtText: "im in my own world"},
-  { username: 'glorr',thoughtText: "im in my own world" },
+  { username: 'tyythegoon', email: "tyythegoon@gmail.com"},
+  { username: 'enphanamus',email: "enphann@gmail.com"},
+  { username: '2cups',email: "2good22@gmail.com"},
+  { username: 'diamond2flatt' ,email: "presssureee@gmail.com"},
+  { username: 'gotmonion',email: "monionnnn@gmail.com" },
+  { username: 'petty',email: "2petty@gmail.com"},
+  { username: 'glorr',email: "gloriaa22@gmail.com" },
 ]);
 
 module.exports = Users;
