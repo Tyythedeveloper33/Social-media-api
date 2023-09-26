424d035f1f70e0f34d3bdac609baf8d809395b68
unknown <kingwebbe@gmail.com>
2023-09-26 03:35:03 -0400

    added constants i will use for thought functionality , now to add functions

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+
+const {
+    getAllThoughts, 
+    getOneThought, // by  _id
+    createNewThought,  
+    updateThought, // by _id
+    deleteThought,    // by _id
+//    **`/api/thoughts/:thoughtId/reactions`**
+   createReaction,
+   deleteReaction
+} = require // file holding thes names for proper functionality
+
+//  api/user
+router.route("/").get(getAllThoughts).post(createNewThought)
+
+router
+.route("/:thoughtId").get(getOneThought).put(updateThought).delete(deleteThought);
+
+router.route("/:userId/friends/:friendId").post(createReaction).delete(deleteReaction);
+
+module.exports = router;
